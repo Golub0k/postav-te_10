3a9c6706d5580180cf19c9c6b539153cccd239e7
Mary Golub <golub202me@gmail.com>
2022-06-09 22:31:03 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,140 +1,113 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include "stdio.h"
-#include "stdlib.h"
-#define SIZE 1000
-#include <string.h>
-#include "structures.h"
-#include "functions.h"
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <map>
+
+void zhadniy(int n, int& sum, int vert, std::vector<std::vector<int>>& spisok, std::vector<int>& visited)
+{
+
+    int town = 0;
+    int min = 2147483647;
+    while (visited.size() != n)//РїРѕСЃРµС‰Р°РµРј РІСЃРµ РіРѕСЂРѕРґР°
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            bool vis = std::find(visited.begin(), visited.end(), j) != visited.end(); //РїСЂРѕРІРµСЂСЏРµРј, РїРѕСЃРµС‰Р°Р»Рё Р»Рё РіРѕСЂРѕРґ
+            if (!vis)
+            {
+                if ((spisok[vert][j] != 0) && (spisok[vert][j] < min)) {
+                    min = spisok[vert][j];
+                    town = j;
+                }
+            }
+        }
+        sum += min;
+        visited.push_back(town);
+        zhadniy(n, sum, town, spisok, visited);
+    }
+
+}
+
+void Vivod(int n, std::vector<std::vector<int>>& matrix)
+{
+    std::vector<std::vector<int>> itog; // РІС‹РІРѕРґ path
+    std::vector<int> price;
+    std::vector<int> chek;// РїРѕСЃРµС‰РµРЅРЅС‹Рµ РіРѕСЂРѕРґР°
+    int summa = 0;
+
+    for (int i = 0; i < n; ++i) //Р°РЅР°Р»РёР· РіРѕСЂРѕРґРѕРІ
+    {
+        chek.push_back(i);
+        zhadniy(n, summa, i, matrix, chek);//РёРґСѓ РІ СЂРµРєСѓСЂСЃРёСЋ
+        int z = chek[chek.size() - 1];
+        if (matrix[z][i] != 0)
+        {
+            chek.push_back(i);
+            summa += matrix[z][i];
+        }
+        price.push_back(summa);
+
+        itog.push_back(chek);
+        chek.clear();
+        summa = 0;
+    }
+
+    int index_min = 0;
+    int min = 2147483647;
+    for (int j = 0; j < itog.size(); ++j) {
+        if (itog[j][0] == itog[j][itog[j].size() - 1]) { // РІ РЅР°С‡Р°Р»Рѕ
+            if ((price[j] > 0) && (price[j] < min)) 
+            {
+                unsigned int x = 0;
+                for (size_t i = 0; i < itog[j].size(); ++i)
+                    for (size_t z = i + 1; z < itog[j].size(); ++z)
+                        if (itog[j][i] == itog[j][z])
+                        {
+                            x += 1;
+                        }
+                if (x == 1) {
+                    min = price[j];    //РјРёРЅ. С†РµРЅР°
+                    index_min = j;
+                }
+            }
+        }
+    }
+    unsigned int vector_size = itog[index_min].size();
+
+    if (min == 2147483647)
+    {
+        std::cout << "Lost" << std::endl;
+    }
+    else {
+        std::cout << "Path:" << std::endl;
+
+        for (auto el : itog[index_min]) { std::cout << el << " "; }
+        std::cout << std::endl;
+        std::cout << "Cost: " << min;
+    }
+}
 
 
 int main()
 {
-    // добавл¤йте пустую строку пожалуйста
-    FILE* input = fopen("input.txt", "rt");
-    printf("Expression:\n");
-    //char* str = (char*)malloc(sizeof(STACK));
-    char str[100] = { 0 };
-    fgets(str, SIZE, input);
-    int len = strlen(str);
-    str[len] = '\0';
-    printf("%s\n", str);
-    STACK* tsifri = NULL;  //стек с числами
-    STACKzn* znaki = 0; //стек со знаками oper
-    char polsk[100] = { 0 };
-    char vivod[100];
-    char variables[30][100] = { 0 };
-    int c = 0; int c2 = 0; int zero = 0;
-    int noprint = 1;
-    while (!feof(input))
+    int n = 0;
+    std::cin >> n;
+    std::vector<std::vector<int>> vse_goroda;
+    int puti = 0;
+    for (int i = 0; i < n; ++i)
     {
-        fgets(variables[c], 100, input);
-        int varlen = strlen(variables[c]);
-        if (varlen != 0)
+        std::vector<int> plus;
+        for (int j = 0; j < n; ++j)
         {
-            variables[c++][varlen - 1] = '\0';
-            printf("%s\n", variables[c - 1]);
+            std::cin >> puti;
+            plus.push_back(puti);
         }
+        vse_goroda.push_back(plus);
+        plus.clear();
     }
-    if (c > 0) {
-        c2 = c - 1;
-        while (c2 != -1)
-        {
-            char str2[1000] = { 0 };
-            int i = 0; int len2 = 0;
-            while (variables[c2][i] != '=')i++; i += 2; int perezap = i; // с этого знака начнем перезаписывать выражение
-            while (variables[c2][i] != '/n' && variables[c2][i] != '/0' && variables[c2][i] != 0) {
-                str2[len2] = variables[c2][i]; len2++; i++;
-            }
-            char variables2[30][100] = { 0 };
-            len2 = strlen(str2);
-            str2[len2] = '\0';
-            STACK* tsifri2 = NULL;  //стек с числами
-            STACKzn* znaki2 = 0; //стек со знаками oper
-            char polsk2[1000] = { 0 };
-            char vivod2[1000];
-            int i2 = 0;
-            for (int i = c2 + 1; i < c; ++i)
-            {
-                strcpy(variables2[i2], variables[i]);
-                i2++;
-            }
-
-            COMPLEX Itog2 = itog(polsk2, vivod2, variables2, str2, len2, tsifri2, znaki2, i2, noprint);//это переменна¤ в комплексном виде, котора¤ была получена из выражени¤
-            if (Itog2.real == 8889 && Itog2.imag == -9998) {
-                zero = 1; break;
-            }
-            // здесь нужно сообразить функцию, котора¤ преобразует Itog2 из double в char и перезаписать строку в variables[c2];
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////!!!!!!!!!!!! нужно обнулить вариэйблз
-            for (int i = perezap; i < 100; i++) { variables[c2][i] = '\0'; }
-            char string_num1[20] = { 0 }; char string_num2[20] = { 0 };
-            Iz_double_v_char(Itog2.real, string_num1); int sn1 = 0;
-            while (string_num1[sn1] != -52 && string_num1[sn1] != '\0')
-            {
-                variables[c2][perezap] = string_num1[sn1];
-                perezap++; sn1++;
-            }
-            if (Itog2.imag < 0)variables[c2][perezap] = '-';
-            else variables[c2][perezap] = '-';
-            perezap++; sn1 = 0;
-            Iz_double_v_char(Itog2.imag, string_num2);
-            while (string_num2[sn1] != -52 && string_num2[sn1] != '\0')
-            {
-                variables[c2][perezap] = string_num2[sn1];
-                perezap++; sn1++;
-            }
-            //string_num1[strlen(string_num1)] = '\0';
-           // printf("%s", string_num1);
-           /* if (Itog2.real < 0)
-            {
-                variables[c2][perezap] = '-';
-                perezap += 1;
-                Itog2.real = -Itog2.real;
-            }*/
-            ///////////////////////////
-            c2 = c2 - 1;
-        }
-    }
-    //int c2 = c-1;
-    //while (c2 >= 0)
-    //{
-    //    char strl[100];
-    //    strcpy(strl, variables[c2]);
-    //    int l = 0; int p = 0; char peremen[30];
-    //    while (strl[l] != '=') { peremen[l] = strl[l]; l++; } l += 2;
-    //    char str2[100];
-    //    int l2 = l;
-    //    while (strl[l2] != '\0' && strl[l2] != '\n') { str2[p] = strl[l2]; p += 1; l2 += 1; }
-    //     int len2 = p; str2[len2] = '\0'; p = 0;
-    //    while (strl[l] != '\0' && strl[l] != '\n') { if (strl[l] == ' ') p += 1; l += 1; }
-    //    if (p > 0)
-    //    {
-    //        int c3 = c2; int c4 = 0;
-    //        char variabels2[30][100];
-    //        while (c3 <= c) { strcpy(variabels2[c4], variables[c3]); c4 += 1; c3 += 1; }
-    //        char polsk2[100] = { 0 };
-    //        char vivod2[100]; STACK* tsifri2 = NULL;  //стек с числами
-    //        STACKzn* znaki2 = 0;
-    //        COMPLEX Lokal_rez= itog(polsk2, vivod2, variabels2, str2, len2, tsifri2, znaki2, c4);
-    //        l = 0;
-    //        while (peremen[l - 1] != '=') { variables[c2][l] = peremen[l]; l + 1; } variables[c2][l] = ' ';
-    //        // нужно переписать строку в с2 в массиве variables, начина¤ с символа l, заполнив ее числом Lokal_rez
 
 
-    //    }
-    //    c2 -= 1;
-    //}
-    noprint = 0;
-    if (zero == 0) {
-        COMPLEX Itog = itog(polsk, vivod, variables, str, len, tsifri, znaki, c, noprint);
-        if (Itog.real == 8889 && Itog.imag == -9998) { printf("correct the input"); }
-        else
-        {
-            if (Itog.imag == 0) {
-                printf("\nResult:\n%.4lf", Itog.real);
-            }
-            else { printf("\nResult:\n%.4lf %.4lfj", Itog.real, Itog.imag); }
-        }
-        /* printf("\nResult:\n%.4lf %.4lfj", Itog.real, Itog.imag);*/
-    }
+    Vivod(n, vse_goroda);
+    return 0;
 }
-
